69f04d3842bd64a947b6c51c35d16f64edf37b95
Felipe Gonzalez <felipe.gonzalez@foursys.com.br>
2022-07-11 10:36:09 -0300

    Iniciando o programa de quebra de um JSON

---
+++ DivisorDeAPIs/DivisorDeAPIs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DivisorDeAPIs
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
