9c7187a0a271e5ed7f208eaf458d5ba1c7ba66d9
Felipe Gonzalez <felipe.gonzalez@foursys.com.br>
2022-07-12 14:29:53 -0300

    Começando uma lógica de manipulação de arquivos

--- DivisorDeAPIs/DivisorDeAPIs/Program.cs
+++ DivisorDeAPIs/DivisorDeAPIs/Program.cs
@@ -4,9 +4,9 @@ namespace DivisorDeAPIs
 {
     public class Program
     {
-        public static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        //public static void Main(string[] args)
+        //{
+        //    var reader =
+        //}
     }
 }
