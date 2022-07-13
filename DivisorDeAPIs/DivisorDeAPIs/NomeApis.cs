74c9013e60c9d7e51017a015aa0940fd3319ebc8
Felipe Gonzalez <felipe.gonzalez@foursys.com.br>
2022-07-13 13:03:47 -0300

    Programa já faz a quebra dos arquivos JSON

---
+++ DivisorDeAPIs/DivisorDeAPIs/NomeApis.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DivisorDeAPIs
+{
+    public static class NomeApis
+    {
+        public const string Login = "Login";
+        public const string Account = "Account";
+        public const string Onboarding = "Onboarding";
+        public const string Customer = "Customer";
+        public const string PersonalAccountCreation = "Personal Account Creation";
+        public const string CompanyAccountCreation = "Company Account Creation";
+        public const string Cards = "Cards";
+        public const string P2P = "P2P";
+        public const string BankTransfer = "Bank Transfer";
+        public const string PaymentSlip = "Payment Slip";
+        public const string CommonFunctionalities = "Common Functionalities";
+    }
+}
--- DivisorDeAPIs/DivisorDeAPIs/Program.cs
+++ DivisorDeAPIs/DivisorDeAPIs/Program.cs
@@ -9,40 +9,8 @@ namespace DivisorDeAPIs
     {
         public static void Main(string[] args)
         {
-            //string readPath = Console.ReadLine();
-            //string readModel = Console.ReadLine();
-            //var paths = obj.paths[readPath];
-            //var components = obj.components.schemas[readModel];
-            //var info = obj.info;
-            //var servers = obj.servers;
-            //var serverb = servers[0].variables.basePath["default"];
-            //serverb = "bolinho";
-
-            //obj.servers[0].variables.basePath["default"] = "bolinho";
-
-            //Console.WriteLine(serverb);
-
-            //var paths = obj.paths;
-
-            //    //var partePrincipal = obj;
-            //    //partePrincipal.paths = null;
-            //    //partePrincipal.components = null;
-
-            //    //partePrincipal.paths = login;
-
-            //    //arquivoLogin = partePrincipal;
-            //    arquivoLogin = "{" + loginTeste + login2Teste + "}";
-            //    //arquivoLogin.info.title = "Login";
-            //}
-
-            //SalvarConteudo(caminhoArquivoNovo, loginTeste, true);
-            //arquivoNovo.WriteLine(partePrincipal[0]);
-            //arquivoNovo.Close();
-            //dynamic conteudo = ObterConteudo(caminhoArquivoNovo);
-
-            string[] nomeArquivos = { "Login", "Onboarding", "Customer", "Personal Account Creation", "Company Account Creation", "Account", "Cards", "P2P", "Bank Transfer", "Payment Slip", "Common Functionalities" };
-
-            dynamic obj = ObterConteudo(@"C:\Users\fgonzalez\Downloads\BlueC Sandbox-v1-oas30 (2).json");
+            string[] nomeArquivos = { NomeApis.Login, NomeApis.Onboarding, NomeApis.Customer, NomeApis.PersonalAccountCreation, NomeApis.CompanyAccountCreation, NomeApis.Account, NomeApis.Cards, NomeApis.P2P, NomeApis.BankTransfer, NomeApis.PaymentSlip, NomeApis.CommonFunctionalities };
+            dynamic obj = ObterConteudo(@"C:\Users\fgonzalez\Downloads\BlueC Sandbox-v1-oas30.json");
             
             string aspas = "\"";
 
@@ -53,40 +21,215 @@ namespace DivisorDeAPIs
             {
                 var caminhoArquivoNovo = @"C:\Users\fgonzalez\Documents\CSU\APIs-Divididas\" + item + ".json";
                 var caminhoAux = @"C:\Users\fgonzalez\Documents\CSU\APIs-Divididas-Aux\" + item + ".json";
-
-                var login = obj.paths["/token"];
-                var loginTeste = aspas + "/token" + aspas + ": " + login + ",";
-                var login2 = obj.paths["/documents/token-device"];
-                var login2Teste = aspas + "/documents/token-device" + aspas + ": " + login2;
-
                 var arquivoNovo = new StreamWriter(caminhoArquivoNovo);
 
-                if (item == "Login")
+                switch (item)
                 {
-                    obj.info.title = "Login";
-                    SalvarConteudo(caminhoAux, obj, true);
+                    case NomeApis.Login:
+                        obj.info.title = NomeApis.Login;
+                        SalvarConteudo(caminhoAux, obj, true);
 
-                    var sr = new StreamReader(caminhoAux);
-                    string arquivoCompleto = sr.ReadToEnd();
+                        var sr = new StreamReader(caminhoAux);
+                        string arquivoCompleto = sr.ReadToEnd();
 
-                    var parteSecundaria = arquivoCompleto.Substring(arquivoCompleto.IndexOf("paths", 0) + 10);
-                    var partePrincipal = arquivoCompleto.Split(parteSecundaria);
+                        var partePrincipal = RetornaPartePrincipalDoArquivo(arquivoCompleto);
 
-                    arquivoNovo.Write(partePrincipal[0]);
-                    arquivoNovo.WriteLine(loginTeste);
-                    arquivoNovo.WriteLine(login2Teste);
-                    arquivoNovo.WriteLine("},");
+                        string[] nomeRotasLogin = { "/token", "/documents/token-device" };
+                        var rotas = RetornaRotasDaApi(nomeRotasLogin, obj, aspas);
+
+                        CriaArquivoJsonCorreto(arquivoNovo, partePrincipal, rotas, componentsFinal);
+                        break;
+                    case NomeApis.Onboarding:
+                        obj.info.title = NomeApis.Onboarding;
+                        SalvarConteudo(caminhoAux, obj, true);
+
+                        sr = new StreamReader(caminhoAux);
+                        arquivoCompleto = sr.ReadToEnd();
+
+                        partePrincipal = RetornaPartePrincipalDoArquivo(arquivoCompleto);
+
+                        string[] nomeRotasOnboarding = { "/documents/agreement" };
+                        rotas = RetornaRotasDaApi(nomeRotasOnboarding, obj, aspas);
+
+                        CriaArquivoJsonCorreto(arquivoNovo, partePrincipal, rotas, componentsFinal);
+                        break;
+                    case NomeApis.Customer:
+                        obj.info.title = NomeApis.Customer;
+                        SalvarConteudo(caminhoAux, obj, true);
+
+                        sr = new StreamReader(caminhoAux);
+                        arquivoCompleto = sr.ReadToEnd();
+
+                        partePrincipal = RetornaPartePrincipalDoArquivo(arquivoCompleto);
+
+                        string[] nomeRotasCustomer = { "/customer/account", "/customer/validate-document" };
+                        rotas = RetornaRotasDaApi(nomeRotasCustomer, obj, aspas);
+
+                        CriaArquivoJsonCorreto(arquivoNovo, partePrincipal, rotas, componentsFinal);
+                        break;
+                    case NomeApis.PersonalAccountCreation:
+                        obj.info.title = NomeApis.PersonalAccountCreation;
+                        SalvarConteudo(caminhoAux, obj, true);
+
+                        sr = new StreamReader(caminhoAux);
+                        arquivoCompleto = sr.ReadToEnd();
+
+                        partePrincipal = RetornaPartePrincipalDoArquivo(arquivoCompleto);
+
+                        string[] nomeRotasPersonalAccountCreation = { "/create-account" };
+                        rotas = RetornaRotasDaApi(nomeRotasPersonalAccountCreation, obj, aspas);
+
+                        CriaArquivoJsonCorreto(arquivoNovo, partePrincipal, rotas, componentsFinal);
+                        break;
+                    case NomeApis.CompanyAccountCreation:
+                        obj.info.title = NomeApis.CompanyAccountCreation;
+                        SalvarConteudo(caminhoAux, obj, true);
+
+                        sr = new StreamReader(caminhoAux);
+                        arquivoCompleto = sr.ReadToEnd();
+
+                        partePrincipal = RetornaPartePrincipalDoArquivo(arquivoCompleto);
+
+                        string[] nomeRotasCompanyAccountCreation = { "/create-account" };
+                        rotas = RetornaRotasDaApi(nomeRotasCompanyAccountCreation, obj, aspas);
+
+                        CriaArquivoJsonCorreto(arquivoNovo, partePrincipal, rotas, componentsFinal);
+                        break;
+                    case NomeApis.Account:
+                        obj.info.title = NomeApis.Account;
+                        SalvarConteudo(caminhoAux, obj, true);
+
+                        sr = new StreamReader(caminhoAux);
+                        arquivoCompleto = sr.ReadToEnd();
+
+                        partePrincipal = RetornaPartePrincipalDoArquivo(arquivoCompleto);
+
+                        string[] nomeRotasAccount = { "/account/{accountId}/balance", "/account/{accountId}/statement" };
+                        rotas = RetornaRotasDaApi(nomeRotasAccount, obj, aspas);
+
+                        CriaArquivoJsonCorreto(arquivoNovo, partePrincipal, rotas, componentsFinal);
+                        break;
+                    case NomeApis.Cards:
+                        obj.info.title = NomeApis.Cards;
+                        SalvarConteudo(caminhoAux, obj, true);
+
+                        sr = new StreamReader(caminhoAux);
+                        arquivoCompleto = sr.ReadToEnd();
+
+                        partePrincipal = RetornaPartePrincipalDoArquivo(arquivoCompleto);
+
+                        string[] nomeRotasCards = { "/card/{accountId}/prepaid/physical/create", "/card/{cardId}", "/card/{cardId}/pin/reset", "/card/customer", "/card/status/update", "/card/status", "/card/next-status" };
+                        rotas = RetornaRotasDaApi(nomeRotasCards, obj, aspas);
+
+                        CriaArquivoJsonCorreto(arquivoNovo, partePrincipal, rotas, componentsFinal);
+                        break;
+                    case NomeApis.P2P:
+                        obj.info.title = NomeApis.P2P;
+                        SalvarConteudo(caminhoAux, obj, true);
+
+                        sr = new StreamReader(caminhoAux);
+                        arquivoCompleto = sr.ReadToEnd();
+
+                        partePrincipal = RetornaPartePrincipalDoArquivo(arquivoCompleto);
+
+                        string[] nomeRotasP2P = { "/transfer/p2p" };
+                        rotas = RetornaRotasDaApi(nomeRotasP2P, obj, aspas);
+
+                        CriaArquivoJsonCorreto(arquivoNovo, partePrincipal, rotas, componentsFinal);
+                        break;
+                    case NomeApis.BankTransfer:
+                        obj.info.title = NomeApis.BankTransfer;
+                        SalvarConteudo(caminhoAux, obj, true);
+
+                        sr = new StreamReader(caminhoAux);
+                        arquivoCompleto = sr.ReadToEnd();
+
+                        partePrincipal = RetornaPartePrincipalDoArquivo(arquivoCompleto);
+
+                        string[] nomeRotasBankTransfer = { "/banktransfer" };
+                        rotas = RetornaRotasDaApi(nomeRotasBankTransfer, obj, aspas);
+
+                        CriaArquivoJsonCorreto(arquivoNovo, partePrincipal, rotas, componentsFinal);
+                        break;
+                    case NomeApis.PaymentSlip:
+                        obj.info.title = NomeApis.PaymentSlip;
+                        SalvarConteudo(caminhoAux, obj, true);
+
+                        sr = new StreamReader(caminhoAux);
+                        arquivoCompleto = sr.ReadToEnd();
+
+                        partePrincipal = RetornaPartePrincipalDoArquivo(arquivoCompleto);
+
+                        string[] nomeRotasPaymentSlip = { "/recharge/payment/slip/{id}", "/recharge/payment/slip", "/payment/slip/create", "/payment/slip/validate" };
+                        rotas = RetornaRotasDaApi(nomeRotasPaymentSlip, obj, aspas);
+
+                        CriaArquivoJsonCorreto(arquivoNovo, partePrincipal, rotas, componentsFinal);
+                        break;
+                    case NomeApis.CommonFunctionalities:
+                        obj.info.title = NomeApis.CommonFunctionalities;
+                        SalvarConteudo(caminhoAux, obj, true);
+
+                        sr = new StreamReader(caminhoAux);
+                        arquivoCompleto = sr.ReadToEnd();
+
+                        partePrincipal = RetornaPartePrincipalDoArquivo(arquivoCompleto);
+
+                        string[] nomeRotasCommonFunctionalities = { "/location/province/BR", "/location/province/BR/{provinceId}", "/location/zipcode/{zipcode}", "/location/bank/branch-id", "/location/banks/BR", "/location/document-agencies/BR", "/location/company/economic-activities", "/location/company/company-nature" };
+                        rotas = RetornaRotasDaApi(nomeRotasCommonFunctionalities, obj, aspas);
+
+                        CriaArquivoJsonCorreto(arquivoNovo, partePrincipal, rotas, componentsFinal);
+                        break;
+                    default:
+                        throw new Exception("Essa API não está contemplada no switch.");
                 }
-
-                arquivoNovo.WriteLine(componentsFinal);
-                arquivoNovo.WriteLine("}");
-                arquivoNovo.Close();
             }
         }
 
-        private string RetornaArquivo()
+        private static dynamic[] RetornaRotasDaApi(string[] nomeRotas, dynamic obj, string aspas)
         {
-            throw new NotImplementedException();
+            dynamic[] rotas = new dynamic[nomeRotas.Length];
+
+            for (var i = 0; i < nomeRotas.Length; i++)
+            {
+                var metodo = obj.paths[nomeRotas[i]];
+                dynamic metodoCorreto = null;
+
+                if (i == nomeRotas.Length - 1)
+                {
+                    metodoCorreto = aspas + nomeRotas[i] + aspas + ": " + metodo;
+                }
+                else
+                {
+                    metodoCorreto = aspas + nomeRotas[i] + aspas + ": " + metodo + ",";
+                }
+
+                rotas.SetValue(metodoCorreto, i);
+            }
+
+            return rotas;
+        }
+
+        private static string[] RetornaPartePrincipalDoArquivo(string arquivoCompleto)
+        {
+            var parteSecundaria = arquivoCompleto.Substring(arquivoCompleto.IndexOf("paths", 0) + 10);
+            var partePrincipal = arquivoCompleto.Split(parteSecundaria);
+
+            return partePrincipal;
+        }
+
+        private static void CriaArquivoJsonCorreto(StreamWriter arquivoNovo ,string[] partePrincipal, dynamic[] rotas, dynamic components)
+        {
+            arquivoNovo.Write(partePrincipal[0]);
+
+            foreach (var item in rotas)
+            {
+                arquivoNovo.WriteLine(item);
+            }
+
+            arquivoNovo.WriteLine("},");
+            arquivoNovo.WriteLine(components);
+            arquivoNovo.WriteLine("}");
+            arquivoNovo.Close();
         }
 
         private static dynamic ObterConteudo(string caminhoArquivo)
