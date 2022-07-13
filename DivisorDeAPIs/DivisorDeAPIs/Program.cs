2d4b69afe5a099ad88c9444de26d43adc4dab365
Felipe Gonzalez <felipe.gonzalez@foursys.com.br>
2022-07-13 11:26:34 -0300

    Login já está funcionando

--- DivisorDeAPIs/DivisorDeAPIs/Program.cs
+++ DivisorDeAPIs/DivisorDeAPIs/Program.cs
@@ -1,12 +1,105 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace DivisorDeAPIs
 {
     public class Program
     {
-        //public static void Main(string[] args)
-        //{
-        //    var reader =
-        //}
+        public static void Main(string[] args)
+        {
+            //string readPath = Console.ReadLine();
+            //string readModel = Console.ReadLine();
+            //var paths = obj.paths[readPath];
+            //var components = obj.components.schemas[readModel];
+            //var info = obj.info;
+            //var servers = obj.servers;
+            //var serverb = servers[0].variables.basePath["default"];
+            //serverb = "bolinho";
+
+            //obj.servers[0].variables.basePath["default"] = "bolinho";
+
+            //Console.WriteLine(serverb);
+
+            //var paths = obj.paths;
+
+            //    //var partePrincipal = obj;
+            //    //partePrincipal.paths = null;
+            //    //partePrincipal.components = null;
+
+            //    //partePrincipal.paths = login;
+
+            //    //arquivoLogin = partePrincipal;
+            //    arquivoLogin = "{" + loginTeste + login2Teste + "}";
+            //    //arquivoLogin.info.title = "Login";
+            //}
+
+            //SalvarConteudo(caminhoArquivoNovo, loginTeste, true);
+            //arquivoNovo.WriteLine(partePrincipal[0]);
+            //arquivoNovo.Close();
+            //dynamic conteudo = ObterConteudo(caminhoArquivoNovo);
+
+            string[] nomeArquivos = { "Login", "Onboarding", "Customer", "Personal Account Creation", "Company Account Creation", "Account", "Cards", "P2P", "Bank Transfer", "Payment Slip", "Common Functionalities" };
+
+            dynamic obj = ObterConteudo(@"C:\Users\fgonzalez\Downloads\BlueC Sandbox-v1-oas30 (2).json");
+            
+            string aspas = "\"";
+
+            var components = obj.components;
+            var componentsFinal = aspas + "components" + aspas + ": " + components;
+
+            foreach (var item in nomeArquivos)
+            {
+                var caminhoArquivoNovo = @"C:\Users\fgonzalez\Documents\CSU\APIs-Divididas\" + item + ".json";
+                var caminhoAux = @"C:\Users\fgonzalez\Documents\CSU\APIs-Divididas-Aux\" + item + ".json";
+
+                var login = obj.paths["/token"];
+                var loginTeste = aspas + "/token" + aspas + ": " + login + ",";
+                var login2 = obj.paths["/documents/token-device"];
+                var login2Teste = aspas + "/documents/token-device" + aspas + ": " + login2;
+
+                var arquivoNovo = new StreamWriter(caminhoArquivoNovo);
+
+                if (item == "Login")
+                {
+                    obj.info.title = "Login";
+                    SalvarConteudo(caminhoAux, obj, true);
+
+                    var sr = new StreamReader(caminhoAux);
+                    string arquivoCompleto = sr.ReadToEnd();
+
+                    var parteSecundaria = arquivoCompleto.Substring(arquivoCompleto.IndexOf("paths", 0) + 10);
+                    var partePrincipal = arquivoCompleto.Split(parteSecundaria);
+
+                    arquivoNovo.Write(partePrincipal[0]);
+                    arquivoNovo.WriteLine(loginTeste);
+                    arquivoNovo.WriteLine(login2Teste);
+                    arquivoNovo.WriteLine("},");
+                }
+
+                arquivoNovo.WriteLine(componentsFinal);
+                arquivoNovo.WriteLine("}");
+                arquivoNovo.Close();
+            }
+        }
+
+        private string RetornaArquivo()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static dynamic ObterConteudo(string caminhoArquivo)
+        {
+            string texto = File.ReadAllText(caminhoArquivo);
+            return JsonConvert.DeserializeObject(texto);
+        }
+
+        private static void SalvarConteudo(string caminhoArquivo, object? conteudo, bool identar)
+        {
+            Formatting formatacao = identar ? Formatting.Indented : Formatting.None;
+            string texto = JsonConvert.SerializeObject(conteudo, formatacao);
+            File.WriteAllText(caminhoArquivo, texto);
+        }
     }
 }
--- DivisorDeAPIs/DivisorDeAPIs/Teste.cs
+++ DivisorDeAPIs/DivisorDeAPIs/Teste.cs
@@ -1,110 +1,110 @@
-﻿using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using Newtonsoft.Json;
+//using System;
+//using System.Collections.Generic;
+//using System.IO;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace DivisorDeAPIs
-{
-    public class Teste
-    {
-        public static void Main(string[] args)
-        {
-            var sr = new StreamReader(@"C:\Users\fgonzalez\Downloads\BlueC Sandbox-v1-oas30 (2).json");
-            string arquivoCompleto = sr.ReadToEnd();
+//namespace DivisorDeAPIs
+//{
+//    public class Teste
+//    {
+//        public static void Main(string[] args)
+//        {
+//            var sr = new StreamReader(@"C:\Users\fgonzalez\Downloads\BlueC Sandbox-v1-oas30 (2).json");
+//            string arquivoCompleto = sr.ReadToEnd();
 
-            string[] nomeArquivos = {"Login", "Onboarding", "Customer", "Personal Account Creation", "Company Account Creation", "Account", "Cards", "P2P", "Bank Transfer", "Payment Slip", "Common Functionalities"};
+//            string[] nomeArquivos = {"Login", "Onboarding", "Customer", "Personal Account Creation", "Company Account Creation", "Account", "Cards", "P2P", "Bank Transfer", "Payment Slip", "Common Functionalities"};
 
-            //Regex prop = new("[a-z]{4,9}[\\s][a-zA-Z]{1,10}[\\s][_]{0,1}[a-zA-Z]{0,9}[\\s]{0,}[_]{0,1}[a-zA-Z]{0,30}[\\s]{0,1}[;|{|=][\\s]{0,}[a-zA-Z1-9]");
-            //MatchCollection p = prop.Matches(caminho);
-            //Console.WriteLine("Total de propriedades: {0}", p.Count);
+//            //Regex prop = new("[a-z]{4,9}[\\s][a-zA-Z]{1,10}[\\s][_]{0,1}[a-zA-Z]{0,9}[\\s]{0,}[_]{0,1}[a-zA-Z]{0,30}[\\s]{0,1}[;|{|=][\\s]{0,}[a-zA-Z1-9]");
+//            //MatchCollection p = prop.Matches(caminho);
+//            //Console.WriteLine("Total de propriedades: {0}", p.Count);
 
-            //Console.WriteLine(arquivo);
-            var parteSecundaria = arquivoCompleto.Substring(arquivoCompleto.IndexOf("paths", 0) + 10);
-            //Console.WriteLine(arquivo);
-            var partePrincipal = arquivoCompleto.Split(parteSecundaria);
+//            //Console.WriteLine(arquivo);
+//            var parteSecundaria = arquivoCompleto.Substring(arquivoCompleto.IndexOf("paths", 0) + 10);
+//            //Console.WriteLine(arquivo);
+//            var partePrincipal = arquivoCompleto.Split(parteSecundaria);
 
-            var aux = arquivoCompleto.IndexOf("/token-device");
-            var aux2 = arquivoCompleto.IndexOf("},", arquivoCompleto.IndexOf("200", aux));
+//            var aux = arquivoCompleto.IndexOf("/token-device");
+//            var aux2 = arquivoCompleto.IndexOf("},", arquivoCompleto.IndexOf("200", aux));
 
-            Console.WriteLine(aux);
-            Console.WriteLine(aux2);
+//            Console.WriteLine(aux);
+//            Console.WriteLine(aux2);
 
-            var parteLogin = arquivoCompleto.Substring(arquivoCompleto.IndexOf("/documents/token-device", arquivoCompleto.IndexOf("paths")) - 1);
-            var parteLogin2 = arquivoCompleto.Substring(aux2 + 2);
+//            var parteLogin = arquivoCompleto.Substring(arquivoCompleto.IndexOf("/documents/token-device", arquivoCompleto.IndexOf("paths")) - 1);
+//            var parteLogin2 = arquivoCompleto.Substring(aux2 + 2);
 
-            var teste = parteLogin.Split(parteLogin2);
+//            var teste = parteLogin.Split(parteLogin2);
 
-            var parte2Login = arquivoCompleto.Substring(arquivoCompleto.IndexOf("/token\"", 0) - 1);
+//            var parte2Login = arquivoCompleto.Substring(arquivoCompleto.IndexOf("/token\"", 0) - 1);
 
-            var auxParte2 = arquivoCompleto.IndexOf("/token\"");
-            var aux2Parte2 = arquivoCompleto.IndexOf("},", arquivoCompleto.IndexOf("500", auxParte2));
+//            var auxParte2 = arquivoCompleto.IndexOf("/token\"");
+//            var aux2Parte2 = arquivoCompleto.IndexOf("},", arquivoCompleto.IndexOf("500", auxParte2));
 
-            var parte2Login2 = arquivoCompleto.Substring(aux2Parte2 + 1);
+//            var parte2Login2 = arquivoCompleto.Substring(aux2Parte2 + 1);
 
-            var teste2 = parte2Login.Split(parte2Login2);
+//            var teste2 = parte2Login.Split(parte2Login2);
 
-            Console.WriteLine("Começando o primeiro arquivo!!");
-            Console.WriteLine(parte2Login);
+//            Console.WriteLine("Começando o primeiro arquivo!!");
+//            Console.WriteLine(parte2Login);
 
-            Console.WriteLine("--------------------------------------------------------------------------------------------------------------------");
-            Console.WriteLine("Começando o segundo arquivo!!");
-            Console.WriteLine(parte2Login2);
+//            Console.WriteLine("--------------------------------------------------------------------------------------------------------------------");
+//            Console.WriteLine("Começando o segundo arquivo!!");
+//            Console.WriteLine(parte2Login2);
 
-            Console.WriteLine("--------------------------------------------------------------------------------------------------------------------");
-            Console.WriteLine("Começando o terceiro arquivo!!");
-            Console.WriteLine(teste2[0]);
+//            Console.WriteLine("--------------------------------------------------------------------------------------------------------------------");
+//            Console.WriteLine("Começando o terceiro arquivo!!");
+//            Console.WriteLine(teste2[0]);
 
-            //Regex met = new("[\\w]{6,9}[\\s][\\w]{3,9}[\\s]{1}[\\w]{1,50}[(][\\s\\S]{0,50}[)][\\s]{0,30}{[\\s]{0,3}");
-            //MatchCollection m = met.Matches(caminho);
-            //Console.WriteLine("Total de metódos: {0}", m.Count);
+//            //Regex met = new("[\\w]{6,9}[\\s][\\w]{3,9}[\\s]{1}[\\w]{1,50}[(][\\s\\S]{0,50}[)][\\s]{0,30}{[\\s]{0,3}");
+//            //MatchCollection m = met.Matches(caminho);
+//            //Console.WriteLine("Total de metódos: {0}", m.Count);
 
-            var parteFinal = arquivoCompleto.Substring(arquivoCompleto.IndexOf("components\"", 0) - 1);
+//            var parteFinal = arquivoCompleto.Substring(arquivoCompleto.IndexOf("components\"", 0) - 1);
 
-            foreach (var item in nomeArquivos)
-            {
-                var caminhoArquivoNovo = @"C:\Users\fgonzalez\Documents\CSU\APIs-Divididas\" + item + ".json";
+//            foreach (var item in nomeArquivos)
+//            {
+//                var caminhoArquivoNovo = @"C:\Users\fgonzalez\Documents\CSU\APIs-Divididas\" + item + ".json";
 
-                var arquivoNovo = new StreamWriter(caminhoArquivoNovo);
+//                var arquivoNovo = new StreamWriter(caminhoArquivoNovo);
 
-                arquivoNovo.WriteLine(partePrincipal[0]);
-                //arquivoNovo.Close();
-                //dynamic conteudo = ObterConteudo(caminhoArquivoNovo);
+//                arquivoNovo.WriteLine(partePrincipal[0]);
+//                //arquivoNovo.Close();
+//                //dynamic conteudo = ObterConteudo(caminhoArquivoNovo);
 
-                if (item == "Login")
-                {
-                    arquivoNovo.WriteLine(teste[0]);
-                    arquivoNovo.WriteLine(teste2[0]);
-                    arquivoNovo.WriteLine("},");
-                    //conteudo["info"]["title"] = "Login";
-                }
+//                if (item == "Login")
+//                {
+//                    arquivoNovo.WriteLine(teste[0]);
+//                    arquivoNovo.WriteLine(teste2[0]);
+//                    arquivoNovo.WriteLine("},");
+//                    //conteudo["info"]["title"] = "Login";
+//                }
 
-                arquivoNovo.WriteLine(parteFinal);
-                //arquivoNovo.WriteLine("}");
-                arquivoNovo.Close();
+//                arquivoNovo.WriteLine(parteFinal);
+//                //arquivoNovo.WriteLine("}");
+//                arquivoNovo.Close();
 
-                //SalvarConteudo(caminhoArquivoNovo, conteudo, true);
-            }
-        }
+//                //SalvarConteudo(caminhoArquivoNovo, conteudo, true);
+//            }
+//        }
 
-        private string RetornaArquivo()
-        {
-            throw new NotImplementedException();
-        }
+//        private string RetornaArquivo()
+//        {
+//            throw new NotImplementedException();
+//        }
 
-        private static dynamic ObterConteudo(string caminhoArquivo)
-        {
-            string texto = File.ReadAllText(caminhoArquivo);
-            return JsonConvert.DeserializeObject(texto);
-        }
+//        private static dynamic ObterConteudo(string caminhoArquivo)
+//        {
+//            string texto = File.ReadAllText(caminhoArquivo);
+//            return JsonConvert.DeserializeObject(texto);
+//        }
 
-        private static void SalvarConteudo(string caminhoArquivo, dynamic conteudo, bool identar)
-        {
-            Formatting formatacao = identar ? Formatting.Indented : Formatting.None;
-            string texto = JsonConvert.SerializeObject(conteudo, formatacao);
-            File.WriteAllText(caminhoArquivo, texto);
-        }
-    }
-}
+//        private static void SalvarConteudo(string caminhoArquivo, dynamic conteudo, bool identar)
+//        {
+//            Formatting formatacao = identar ? Formatting.Indented : Formatting.None;
+//            string texto = JsonConvert.SerializeObject(conteudo, formatacao);
+//            File.WriteAllText(caminhoArquivo, texto);
+//        }
+//    }
+//}
